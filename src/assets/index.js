5dcba7e43176c5349c261fdb9356c8fcb7cf33c4
Andrew <awcrotwell@gmail.com>
2021-01-23 11:03:26 -0500

    DOMContentLoaded

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,52 +1,54 @@
-(function () {
-  this.App || (this.App = {});
+document.addEventListener("DOMContentLoaded", () => {
+  (function () {
+    this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer(
-    "ws://localhost:3000/cable?token=12345123" // if using the default options
+    App.cable = ActionCable.createConsumer(
+      "ws://localhost:3000/cable?token=12345123" // if using the default options
+    );
+  }.call(this));
+
+  App.channels || (App.channels = {});
+
+  App.channels["chat_1"] = App.cable.subscriptions.create(
+    {
+      channel: "ChatChannel",
+      room: "1",
+    },
+    {
+      connected: function (data) {
+        return console.log("ChatChannel connected", data);
+      },
+      disconnected: function (data) {
+        return console.log("ChatChannel disconnected", data);
+      },
+      received: function (data) {
+        console.log("ChatChannel received", data);
+        const node = document.createElement("P");
+        const textnode = document.createTextNode(data.message);
+        node.appendChild(textnode);
+        document.getElementById("chat-log").appendChild(node);
+        document.getElementById("chat_message").value = "";
+      },
+      rejected: function () {
+        return console.log("ChatChannel rejected");
+      },
+      away: function () {
+        return this.perform("away");
+      },
+      status: function (status) {
+        return this.perform("status", {
+          status: status,
+        });
+      },
+    }
   );
-}.call(this));
 
-App.channels || (App.channels = {});
+  const form = document.querySelector("form");
 
-App.channels["chat_1"] = App.cable.subscriptions.create(
-  {
-    channel: "ChatChannel",
-    room: "1",
-  },
-  {
-    connected: function (data) {
-      return console.log("ChatChannel connected", data);
-    },
-    disconnected: function (data) {
-      return console.log("ChatChannel disconnected", data);
-    },
-    received: function (data) {
-      console.log("ChatChannel received", data);
-      const node = document.createElement("P");
-      const textnode = document.createTextNode(data.message);
-      node.appendChild(textnode);
-      document.getElementById("chat-log").appendChild(node);
-      document.getElementById("chat_message").value = "";
-    },
-    rejected: function () {
-      return console.log("ChatChannel rejected");
-    },
-    away: function () {
-      return this.perform("away");
-    },
-    status: function (status) {
-      return this.perform("status", {
-        status: status,
-      });
-    },
-  }
-);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const msg = document.getElementById("message");
-  App.channels["chat_1"].send({ message: msg.value });
+    const msg = document.getElementById("message");
+    App.channels["chat_1"].send({ message: msg.value });
+  });
 });
